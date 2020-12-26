5e1a9465ff202f23182de2df32d3d4e979c29a70
Rogério Pereira <rogerio.pereira@luizalabs.com>
2020-12-26 17:28:54 -0300

    ajuste settings

--- openme/settings/heroku.py
+++ openme/settings/heroku.py
@@ -12,12 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%n0mz%zr1&%iyiot-tis&s$7alp7yc1g40c%z@o$6_e2gzl4zx'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['openme-production.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['openme-production.herokuapp.com']
 
 
 # Application definition
