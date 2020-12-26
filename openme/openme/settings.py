54c359a9df7b4081bcd5ff4eb14220fa6f60d125
Rogério Pereira <rogerio.pereira@luizalabs.com>
2020-12-26 14:25:30 -0300

    ajustes no setting

--- openme/openme/settings.py
+++ openme/openme/settings.py
@@ -1,4 +1,5 @@
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -108,7 +109,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False
